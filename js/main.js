f8d5de982baa40cabf6471bb5fb78f45f6d607fc
Shakti <mbshakti@users.noreply.github.com>
2020-02-13 17:10:32 -0500

    Add files via upload
    add schedule page

--- js/main.js
+++ js/main.js
@@ -2,16 +2,26 @@ $("#show_schedule_button").click(function() {
     $("#schedule_table_all").fadeIn();
     $("#show_info_all").hide();
     $("#show_faq_all").hide();
+    $("#welcome_text").hide();
 });
 
 $("#show_info_button").click(function() {
     $("#show_info_all").fadeIn();
     $("#schedule_table_all").hide();
     $("#show_faq_all").hide();
+    $("#welcome_text").hide();
 });
 
 $("#show_faq_button").click(function() {
     $("#show_faq_all").fadeIn();
     $("#show_info_all").hide();
     $("#schedule_table_all").hide();
+    $("#welcome_text").hide();
+});
+
+$("#container").click(function() {
+    $("#schedule_table_all").hide();
+    $("#show_info_all").hide();
+    $("#show_faq_all").hide();
+    $("#welcome_text").fadeIn();
 });
